6eca14a646c978fb0e29dc57219f7df2a603a5cb
malena kiryluk <malenakiryluk@gmail.com>
2022-08-01 22:11:26 -0300

    js

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const zapatilla2 = new Producto ("zapatilla", "kyrie 5 bob esponja", "amarillo",
 
 const productos = [remera, remera2, short, short2, zapatilla, zapatilla2];
 
-const categoria = prompt("Bienvenido a Clutchwear, que es lo que esta buscando?")
+const categoria = prompt("Hola bienvendioa clutchwear, que esta buscando?")
+const remeras = document.querySelector("#remeras")
+const shorts = document.querySelector("#shorts")
+const zapatillas = document.querySelector("#zapatillas")
 
 const catProducto = (productos, categoria) => {
 
@@ -22,19 +25,33 @@ const catProducto = (productos, categoria) => {
         const remeras = productos.filter( (elemento) => {
             return elemento.categoria === "remera"
         })
+        
+        shorts.classList.toggle("block")
+        zapatillas.classList.toggle("block")
         console.log(remeras)
+
     }
     else if(categoria === "shorts"){
         const shorts = productos.filter( (elemento) => {
             return elemento.categoria === "short"
         })
+
+        remeras.classList.toggle("block")
+        zapatillas.classList.toggle("block")
         console.log(shorts)
+
+        
     }
     else if(categoria === "zapatillas"){
         const zapatillas = productos.filter( (elemento) => {
             return elemento.categoria === "zapatilla"
         })
+
+        remeras.classList.toggle("block")
+        shorts.classList.toggle("block")
         console.log(zapatillas)
+
+        
     }
     else{
         console.log("no contamos con ese producto :(")
@@ -42,4 +59,3 @@ const catProducto = (productos, categoria) => {
 }
 
 catProducto(productos, categoria);
-
