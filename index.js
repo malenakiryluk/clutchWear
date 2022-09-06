42d017be63e89d286b8afc58590333a73ac5e580
malena kiryluk <malenakiryluk@gmail.com>
2022-09-05 22:24:08 -0300

    carrito

--- index.js
+++ index.js
@@ -30,21 +30,21 @@ form.onsubmit = (event) => {
         remeras.innerHTML = `<h2 class="subtitulo">Remeras:</h2>
 
         <div class="fotoPadre">
-            <div class="fotoUno">
+            <div class="fotoUno item">
                 <img class="foto" src="./imagenes/camisetaChicago.jpg" alt="remera chicago bulls">
                 <figcaption class="pieDeFoto">Remera chicago bulls original de jordan </figcaption>
                 <p>$15.000</p>
                 <div class="d-grid gap-2 col-6 mx-auto">
-                  <button class="btn btn-primary color margin" type="button">Agregar al carrito</button>
+                  <button class="btn btn-primary color margin addCarrito" type="button">Agregar al carrito</button>
                 </div>
             </div>
 
-            <div class="fotoDos">
+            <div class="fotoDos item">
                 <img class="foto" src="./imagenes/remeraRaptors.jpg" alt="remera de los raptors">
                 <figcaption class="pieDeFoto">Remera retro toronto raptors de vince carter</figcaption>
                 <p>$17.000</p>
                 <div class="d-grid gap-2 col-6 mx-auto">
-                  <button class="btn btn-primary color margin" type="button">Agregar al carrito</button>
+                  <button class="btn btn-primary color margin addCarrito" type="button">Agregar al carrito</button>
                 </div>
             </div>
         </div>`
@@ -53,21 +53,21 @@ form.onsubmit = (event) => {
         shorts.innerHTML = `<h2 class="subtitulo">Shorts:</h2>
 
         <div class="fotoPadre">
-            <div class="fotoUno">
+            <div class="fotoUno item">
                 <img class="foto" src="./imagenes/shortArgentina.jpg" alt="short de argentina">
                 <figcaption class="pieDeFoto">Short original de la seleccion argentina</figcaption>
                 <p>$12.000</p>
                 <div class="d-grid gap-2 col-6 mx-auto">
-                  <button class="btn btn-primary color margin" type="button">Agregar al carrito</button>
+                  <button class="btn btn-primary color margin addCarrito" type="button">Agregar al carrito</button>
                 </div>
             </div>
 
-            <div class="fotoDos">
+            <div class="fotoDos item">
                 <img class="foto" src="./imagenes/shortNuggets.jpg" alt="short de denver nuggets">
                 <figcaption class="pieDeFoto">Short original de los denver nuggets</figcaption>
                 <p>$13.000</p>
                 <div class="d-grid gap-2 col-6 mx-auto">
-                  <button class="btn btn-primary color margin" type="button">Agregar al carrito</button>
+                  <button class="btn btn-primary color margin addCarrito" type="button">Agregar al carrito</button>
                 </div>
             </div>
         </div>`
@@ -76,23 +76,23 @@ form.onsubmit = (event) => {
         zapatillas.innerHTML = `<h2 class="subtitulo">Zapatillas:</h2>
 
         <div class="fotoPadre">
-            <div class="fotoUno">
+            <div class="fotoUno item">
                 <img class="foto" src="./imagenes/zapatillaJordan.png" alt="zapatillas jordan">
                 <figcaption class="pieDeFoto">Zapatillas nike jordan 1 low travis scott</figcaption>
                 <p>$40.000</p>
                 <div class="d-grid gap-2 col-6 mx-auto">
-                  <button class="btn btn-primary color margin" type="button">Agregar al carrito</button>
+                  <button class="btn btn-primary color margin addCarrito" type="button">Agregar al carrito</button>
                 </div>
                 
             </div>
 
-            <div class="fotoDos">
+            <div class="fotoDos item">
 
                 <img class="foto" id="zapatilla" src="./imagenes/zapatillasKyrie.jpg" alt="zapatillas kyrie">
                 <figcaption class="pieDeFoto">Zapatillas kyrie 5 edicion bob esponja</figcaption>
                 <p>$35.000</p>
                 <div class="d-grid gap-2 col-6 mx-auto">
-                  <button class="btn btn-primary color margin" type="button">Agregar al carrito</button>
+                  <button class="btn btn-primary color margin addCarrito" type="button">Agregar al carrito</button>
                 </div>
 
             </div>
@@ -176,5 +176,35 @@ envio.onclick = ()=>{
 }
 
 
+// CARRITO
 
+const agregarCarritoBtn = document.querySelectorAll(".addCarrito")
+const carritoContent = document.querySelector(".carritoContent")
+console.log( agregarCarritoBtn, carritoContent)
+
+agregarCarritoBtn.forEach( agregarBtn => {
+
+  agregarBtn.addEventListener('click', agregarCarritoClick)
+})
+
+
+
+function agregarCarritoClick(event){
+  const btn = event.target;
+  const item = btn.closest(".item");
+
+  const itemNombre = item.querySelector(".pieDeFoto").textContent;
+  const itemPrecio = item.querySelector("p").textContent;
+  const itemFoto = item.querySelector(".foto").src;
+
+  agregarAlCarrito(itemNombre, itemPrecio, itemFoto)
+}
+
+
+function agregarAlCarrito(itemNombre, itemPrecio,itemFoto){
+
+  carritoContent.innerHTML =`<img src="${itemFoto} class="img-fluid foto" alt="zapatillas jordan 1 low travis scott">
+  <figcaption class="pieDeFoto carritoColor">${itemNombre}</figcaption>
+  <p class="carritoColor">${itemPrecio}</p>`
+}
 
